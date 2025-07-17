67c8063be1b7f06ad5e1a7e48c784ee7f6c140a0
mangoyoo <676224061@qq.com>
2025-07-17 14:55:07 +0800

    框架

--- src/main/java/com/mangoyoo/selfaiagent/Manus/MyManus.java
+++ src/main/java/com/mangoyoo/selfaiagent/Manus/MyManus.java
@@ -14,13 +14,15 @@ public class MyManus extends ToolCallAgent {
         String SYSTEM_PROMPT = """
                 You are YuManus, an all-capable AI assistant, aimed at solving any task presented by the user.
                 You have various tools at your disposal that you can call upon to efficiently complete complex requests.
+                Default to reply in Chinese.
                 """;
         this.setSystemPrompt(SYSTEM_PROMPT);
         String NEXT_STEP_PROMPT = """
                 Based on user needs, proactively select the most appropriate tool or combination of tools.
                 For complex tasks, you can break down the problem and use different tools step by step to solve it.
+                If you want to stop the interaction at any point, use the `terminate` tool/function call.
+                Every conversation response must call the tool! Also, think about why this should be done. Just describe the functionality without telling the user the specific tool name
                 After using each tool, clearly explain the execution results and suggest the next steps.
-                By default, the next step is required before the user's goal is achieved.
                 If you want to stop the interaction at any point, use the `terminate` tool/function call.
                 """;
         this.setNextStepPrompt(NEXT_STEP_PROMPT);
